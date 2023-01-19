a67ec03137702a2978a7679c5f53c78e36dbd512
Angel <santoyox714@gmail.com>
2023-01-18 19:46:23 -0800

    day18

---
+++ day18/day18.py
@@ -0,0 +1,32 @@
+#prompt is def not a grid
+#run in processing https://processing.org/
+def setup():
+    size(800,500,P3D)
+    noStroke()
+
+
+def draw():
+    lighting()
+    background(100)
+
+    for x in range(0, width, 20):
+        for y in range(0, height, 20):
+            pushMatrix()
+            translate(x,y)
+            rotateY(map(mouseX,0,width,0,PI))
+            rotateX(map(mouseY,0,height,0,PI))
+            sphere(90) #def not a grid of spheres
+            popMatrix()
+
+def lighting():
+    pointLight(129,205,245, 200,-150,0)#rgb followed by xyz axis
+    directionalLight(1,255,79,1,0,0)
+    spotLight(240,25,232,   #rgb
+            100, 200, 200,      #pos
+            0, -0.5, -0.5,   #dir
+            PI/2, 2)       #angle
+    spotLight(250,229,25,   
+            700, 600, 200,      
+            0, -0.5, -0.5,   
+            PI/2, 2)       
+    
