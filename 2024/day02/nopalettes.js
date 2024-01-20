a349dfb9ac86432a41bd48dffdc952a9aab99546
Angel <santoyox714@gmail.com>
2024-01-19 20:49:51 -0800

    day2

--- 2024/day02/nopalettes.js
+++ 2024/day02/nopalettes.js
@@ -1,13 +1,37 @@
+let maxFrames = 1000; //without a frame count this code would draw splotches 4ever
 
 function setup() {
-  createCanvas(900, 900);
-
-
+  createCanvas(800, 600);
+  noStroke();
 }
 
 function draw() {
-  background(255, 255, 255);
+  if (frameCount <= maxFrames) {
+  let splatterColor = color(random(255), random(255), random(255), random(200, 255));
 
- 
+  let splatterSize = random(20, 50);
+  let splatterX = random(width);
+  let splatterY = random(height);
+
+
+  fill(splatterColor);
+  beginShape();
+
+  for (let i = 0; i < 360; i++) { 
+    //this gives us the splotchy effect by modifying the shape of the splotches and giving us 360 points for the splotch
+    //then randomizing the angle and radius for each splotch
+
+    let angle = random(TWO_PI);
+    let radius = random(splatterSize * 0.5, splatterSize);
+    let offsetX = cos(angle) * radius;
+    let offsetY = sin(angle) * radius;
+    vertex(splatterX + offsetX, splatterY + offsetY);
+  }
+  endShape(CLOSE);
 }
+else
+{
+  noLoop();
+}
+} 
 
